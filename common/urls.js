1d6ce5325f0bc9de580fe6a362d886cbcc84d80f
mik <mik23@protonmail.ch>
2021-05-23 10:18:07 +0530

    common urls used across site

---
+++ common/urls.js
@@ -0,0 +1,7 @@
+export const WEBSITE = "https://mik.sh";
+
+export const GITHUB = "https://github.com/Mik1337";
+
+export const TWITTER = "https://twitter.com/AvocadoMik";
+
+export const TWITCH = "https://twitch.tv/u/AvocadoMik/";
