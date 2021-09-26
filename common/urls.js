cbc9810cf2caf5f6f5162955ff6ee3562293bcec
mik <mik23@protonmail.ch>
2021-09-27 04:28:28 +0530

    modified:   common/urls.js

--- common/urls.js
+++ common/urls.js
@@ -5,3 +5,5 @@ export const GITHUB = "https://github.com/Mik1337";
 export const TWITTER = "https://twitter.com/AvocadoMik";
 
 export const TWITCH = "https://twitch.tv/u/AvocadoMik/";
+
+export const CAFFEINE = "https://www.caffeine.tv/Mik23";
