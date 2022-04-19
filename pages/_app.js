754dac6d26da0d8f1ef907ea46a3b99dd917333f
mik <mik23@protonmail.ch>
2022-04-20 04:53:02 +0530

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,22 @@ const WebApp = ({ Component, pageProps }) => {
     <>
       <Global styles={globalCSS} />
       <Component {...pageProps} />
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-0XY0XBL7FZ `}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-0XY0XBL7FZ ', {
+             page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </>
   );
 };
